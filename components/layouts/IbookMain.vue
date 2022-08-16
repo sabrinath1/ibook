5338fdabe23344f777e3a206e70c4115b3011832
Sabrina <sabrinath1@hotmail.com>
2022-08-16 18:34:54 -0300

    first commit

---
+++ components/layouts/IbookMain.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="ibook-layout">
+    <ContainerCenter>
+      <Nuxt />
+    </ContainerCenter>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.ibook-layout {
+  margin-top: 4.313rem;
+  display: grid;
+  grid-gap: 6.75rem;
+  .container {
+    padding-bottom: 6.75rem;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="wrapper">
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
+<style lang="scss" scoped>
+.wrapper {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 200px;
+  grid-auto-flow: column;
+  grid-gap: 0.5rem;
+  @include screen('small') {
+    grid-template-columns: 1fr;
+  }
+  @include screen('medium') {
+    grid-template-columns: 1fr 1fr;
+  }
+  @include screen('large', 'infinity') {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+  .box {
+    background-color: color('dark', 'darkest');
+  }
+}
+</style>
