093a0c7e0dec56f0f0915bc967b4425069092d1b
Sabrina <sabrinath1@hotmail.com>
2022-08-17 16:57:56 -0300

    layout ibook

---
+++ layouts/ibook.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="ibook-layout">
+    <Header />
+    <ContainerCenter>
+      <Nuxt />
+    </ContainerCenter>
+    <Footer />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.ibook-layout {
+  margin-top: 4.313rem;
+  display: grid;
+  grid-gap: 6.75rem;
+  .container {
+    padding-bottom: 6.75rem;
+  }
+}
+</style>
