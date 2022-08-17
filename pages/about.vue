f5b0d7a69a63ce5616680987697ee3a4587f9fb4
Sabrina <sabrinath1@hotmail.com>
2022-08-17 16:58:46 -0300

    page about

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <AboutTemplate />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import AboutTemplate from '~/components/templates/AboutTemplate.vue'
+
+export default Vue.extend({
+  layout: 'ibook',
+  components: { AboutTemplate },
+})
+</script>
